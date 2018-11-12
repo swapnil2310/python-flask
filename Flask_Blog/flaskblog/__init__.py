66d050b3ba806097575f23fe2d5ba1fae607149e
swapnil2310 <43606174+swapnil2310@users.noreply.github.com>
2018-11-12 20:17:02 +0530

    Changes related to Login

--- Flask_Blog/flaskblog/__init__.py
+++ Flask_Blog/flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -8,5 +9,8 @@ app.config['SECRET_KEY'] = '39fe1dd9ee4672c0c1bb3c2c048b584c'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
