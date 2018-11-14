541b527ee9bca7592ce11d1bcf68b0cc4265dfa2
swapnil2310 <43606174+swapnil2310@users.noreply.github.com>
2018-11-14 20:51:11 +0530

    Profile pic changes

--- Flask_Blog/flaskblog/forms.py
+++ Flask_Blog/flaskblog/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField , PasswordField  , SubmitField , BooleanField
 from wtforms.validators import DataRequired , Length , Email , EqualTo ,ValidationError
 from flaskblog.models import User
+from flask_login import current_user
+from flask_wtf.file import FileField , FileAllowed
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -29,3 +31,22 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                            validators=[DataRequired() ,Length(min=2 , max=20)] )
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    picture = FileField('Update Profile Picture',validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('This username is taken . Pls choose different one.')
+    
+    def validate_email(self,email):
+        if email.data != current_user.email:    
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('This email is taken . Pls choose different one.')
+    
